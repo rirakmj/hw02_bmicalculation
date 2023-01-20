55347f48efb732305f89f6565a95f7f93fea689e
KIM MINJI <riramj801@gmail.com>
2023-01-20 15:55:58 +0900

    Initial commit

---
+++ app/src/main/java/com/cookandroid/hw02_bmicalculation/MainActivity.java
@@ -0,0 +1,15 @@
+package com.cookandroid.hw02_bmicalculation;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        setTitle("신체 질량 지수 측정");
+    }
+}
